00b4032bca95b9d4cffdd4b7e9575cdb6b3c0e1b
Nate Fabian <natefabian18@gmail.com>
2024-11-18 18:27:45 -0500

    Added database tables, and other basic deps stuff

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -4,3 +4,19 @@ export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
 	age: integer('age')
 });
+
+export const sprint = sqliteTable('sprint', {
+	id: integer('id').primaryKey(),
+	name: text('name'),
+	user_id: integer('user_id').references(() => user.id),
+	created_at: text('created_at'),
+	updated_at: text('updated_at')
+});
+
+export const task = sqliteTable('task', {
+	id: integer('id').primaryKey(),
+	name: text('name'),
+	sprint_id: integer('sprint_id').references(() => sprint.id),
+	created_at: text('created_at'),
+	updated_at: text('updated_at')
+});
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	  
+</script>
+
+<style>
+	/* your styles go here */
+</style>
+
+<slot>Something is fucked</slot>
